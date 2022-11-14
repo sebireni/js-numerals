2b68c1b7a0ee6f37bd3bf148e40e2f1a62aed860
sebireni <sebireni@gmail.com>
2022-11-14 09:14:38 +0100

    completed conversion

--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -5,12 +5,11 @@ import './Form.css';
 const Form = () => {
   const [number, setNumber] = useState(null);
   const [text, setText] = useState('');
-  // const [submit, setSubmit] = useState(false);
   const digits = [];
-  const ones = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
-  const tento19 = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  const prefixes = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
-  const sufixes = ['', 'houndred', 'thousand', 'million', 'billion', 'trillion'];
+  const arrDig = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+  const arrTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+  const arrTy = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  //const sufixes = ['houndred', 'thousand', 'million', 'billion', 'trillion'];
 
   const getNumberValue = (e) => {
     setNumber(Number(e.target.value));
@@ -35,20 +34,13 @@ const Form = () => {
       return;
     };
 
-    if (number>1000000000000) {
-      setText('number must be smaller then 1000000000000');
-      return;
-    };
-
-    if (number < 10) {
-      setText(ones[number]);
-      return;
-    } else if (number < 20) {
-      setText(tento19[number-10]);
+    if (number>1000000000000000) {
+      setText('number must be smaller then 1.000.000.000.000.000');
       return;
     };
 
     var n  = number;
+    var text2 = '';
 
     while (n > 0) {
       digits.push(n % 10);
@@ -56,12 +48,52 @@ const Form = () => {
     }
     digits.reverse();
 
-    console.log(digits);
+    if (number === 0) {
+      text2 = 'zero';
+    } else {
 
+      for (let i=digits.length-1; i >= 0; i--) {
+        
+        if (digits[i] !== 0) {
 
-    // for (let i=digits.length; i>=0; i--) {
+          if (digits.length-i-1 % 3 === 0) {
+            if (digits.length - i === 4) {
+              text2 = ' thousand ' + text2;
+            } else if (digits.length - i === 7) {
+              text2 = ' million ' + text2;
+            } else if (digits.length - i === 10) {
+              text2 = ' billion ' + text2;
+            } else if (digits.length - i === 13) {
+              text2 = ' trillion ' + text2;
+            }
 
-    // };
+            if (digits[i-1] === 1) { //amikor i 0 azt kezelni meg!?
+              text2 = arrTeen[digits[i]] + text2;
+            } else {
+              text2 = arrDig[digits[i]] + text2;
+            };
+          
+          } else if (digits.length-i-1 % 3 === 1) {
+            if (digits[i+1] > 0) {
+              text2 = arrTy[digits[i]-2] + '-' + text2;
+            } else {
+              text2 = arrTy[digits[i]-2] + text2;
+            }
+
+          } else if (digits.length-i-1 % 3 === 2) {
+            if (digits[digits.length-1] !== 0 || digits[digits.length-2] !== 0) {
+              text2 = arrDig[digits[i]] + ' houndred and ' + text2;
+            } else {
+              text2 = arrDig[digits[i]] + ' houndred' + text2;
+            }
+          }
+        }
+        console.log(text2); // 3. kör után nem ad hozza a text2höz!?   
+        //kotojel meg hianyzik!
+      }
+    };
+
+    setText(text2);
   };
 
   return (
