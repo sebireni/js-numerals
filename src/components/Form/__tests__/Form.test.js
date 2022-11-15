e952c59202dca783831e864772669326ebb5b20f
sebireni <sebireni@gmail.com>
2022-11-15 11:09:21 +0100

    fixed converison

--- src/components/Form/__tests__/Form.test.js
+++ src/components/Form/__tests__/Form.test.js
@@ -4,32 +4,10 @@ import Form from './../index';
 import { render, screen, cleanup } from '@testing-library/react';
 import renderer from 'react-test-renderer';
 //import 'jest-dom/extend-expect';
+//import convert from
 
-//v2
-// afterEach(cleanup);
 
-// it('renders without crashing', () => {
-//     const div = document.createElement('div');
-//     ReactDOM.render(<Form></Form>, div);
-// });
-
-// it('renders form correctly', () => {
-//     const {getByTestId} = render(<Form prop='testingtasting'></Form>);
-//     expect(getByTestId('form1')).toHaveTextContent('testingtesting'); //?
-//     expect(screen.getByTestId('form1')).toHaveTextContent('testingtesting'); //?
-
-// });
-
-//v1
-test('should render form component', () => {
-    render(<Form/>);
-    const FormElement = screen.getByTestId('form1');
-    expect(FormElement).toBeinTheDocument();
-    expect(FormElement).toHaveTextContent('Convert');
-});
-
-// test('matches snapshot', () => {
-//     const form = {};
-//     const tree = renderer.create(<Form form={}/>).toJSON();
-//     expect(tree).toMatchSnapshot();
-// });
+// test('conversion 42', () =>  {
+//     const convertedNumber = convert(42);
+//     expect(convertedNumber).toEqual('fortytwo');
+// } )
--- src/components/Form/index.js
+++ src/components/Form/index.js
@@ -9,7 +9,7 @@ const Form = () => {
   const digits = [];
   const arrDig = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
   const arrTeen = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
-  const arrTy = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+  const arrTy = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
 
   const getNumberValue = (e) => {
     setNumber(Number(e.target.value));
@@ -21,7 +21,7 @@ const Form = () => {
   };
 
   const validator = () => {
-    if (number < 0 || !Number.isInteger(number) || number>1000000000000) {
+    if (number < 0 || !Number.isInteger(number) || number>1000000000000000) {
       alert('number must be positive and integer and smaller then 1000000000000');
       return;
     }
@@ -43,27 +43,39 @@ const Form = () => {
       return;
     };
 
-    var n  = number;
-    var text2 = '';
+    let n  = number;
+    let text2 = '';
+
+    if (number < 10) {
+      setText(arrDig[number]);
+      return;
+    } else if (number < 20) {
+      setText(arrTeen[number-10]);
+      return;
+    };
 
     while (n > 0) {
       digits.push(n % 10);
       n = Math.trunc(n / 10);
     }
-    digits.reverse();
+    // digits.reverse();
 
     if (british && number > 1000 && number < 2000) {
-      text2 = arrTeen[digits[1]] + ' hundred and ';
-      if (digits[2] === 1) {
-        text2 = text2 + arrTeen[digits[3]];
-      } else if (digits[2] === 0) {
-        if (digits[3] !== 0) {
-          text2 = text2 + arrDig[digits[3]];
-        }
-      } else if (digits[3] === 0) {
-        text2 = text2 + arrTy[digits[2]-2];
+      if (digits[0] !== 0 || digits[1] !==0) {
+        text2 = arrTeen[digits[2]] + ' hundred and ';
       } else {
-        text2 = text2 + arrTy[digits[2]-2] + '-' + arrDig[digits[3]];
+        text2 = arrTeen[digits[2]] + ' hundred';
+      }
+      if (digits[1] === 1) {
+        text2 = text2 + arrTeen[digits[0]];
+      } else if (digits[1] === 0) {
+        if (digits[0] !== 0) {
+          text2 = text2 + arrDig[digits[0]];
+        }
+      } else if (digits[0] === 0) {
+        text2 = text2 + arrTy[digits[1]-2];
+      } else {
+        text2 = text2 + arrTy[digits[1]-2] + '-' + arrDig[digits[0]];
       }
     } else {
 
@@ -71,47 +83,53 @@ const Form = () => {
         text2 = 'zero';
       } else {
 
-        for (let i=digits.length-1; i >= 0; i--) {
+        for (let i=0; i < digits.length; i++) {
+
+          if (i % 3 === 0) {
+            if ( i === 3) {
+              text2 = ' thousand ' + text2;
+            } else if (i === 6) {
+              text2 = ' million ' + text2;
+            } else if (i === 9) {
+              text2 = ' billion ' + text2;
+            } else if (i === 12) {
+              text2 = ' trillion ' + text2;
+            }
+          }
           
           if (digits[i] !== 0) {
 
-            if (digits.length-i-1 % 3 === 0) {
-              if (digits.length - i === 4) {
-                text2 = ' thousand ' + text2;
-              } else if (digits.length - i === 7) {
-                text2 = ' million ' + text2;
-              } else if (digits.length - i === 10) {
-                text2 = ' billion ' + text2;
-              } else if (digits.length - i === 13) {
-                text2 = ' trillion ' + text2;
-              }
-
-              if (digits[i-1] === 1) { //amikor i 0 azt kezelni meg!?
+            if (i % 3 === 0) {
+              if (i < digits.length && digits[i+1] === 1) {
                 text2 = arrTeen[digits[i]] + text2;
               } else {
                 text2 = arrDig[digits[i]] + text2;
               };
             
-            } else if (digits.length-i-1 % 3 === 1) {
-              if (digits[i+1] > 0) {
-                text2 = arrTy[digits[i]-2] + '-' + text2;
-              } else {
-                text2 = arrTy[digits[i]-2] + text2;
+            } else if (i % 3 === 1) {
+              if (digits[i] !== 1) {
+                if (digits[i-1] > 0) {
+                  text2 = arrTy[digits[i]-2] + '-' + text2;
+                } else {
+                  text2 = arrTy[digits[i]-2] + text2;
+                }
               }
 
-            } else if (digits.length-i-1 % 3 === 2) {
-              if (digits[digits.length-1] !== 0 || digits[digits.length-2] !== 0) {
-                text2 = arrDig[digits[i]] + ' houndred and ' + text2;
+            } else if (i % 3 === 2) {
+              if (digits[i-1] !== 0 || digits[i-2] !== 0) {
+                text2 = arrDig[digits[i]] + ' hundred and ' + text2;
               } else {
-                text2 = arrDig[digits[i]] + ' houndred' + text2;
+                text2 = arrDig[digits[i]] + ' hundred' + text2;
               }
             }
           }
-          console.log(text2); // 3. kör után nem ad hozza a text2höz!?   
+
+          if (i === 2 && digits.length > 3 && digits[2] === 0) {
+            text2 = ' and ' + text2;
+          }
         }
       }
     }
-
     setText(text2);
   };
 
@@ -134,7 +152,7 @@ const Form = () => {
           id='British'
           onChange={toggleBritish}
         />
-        <label for='British'>British English counting</label>
+        <label htmlFor='British'>British English counting</label>
       </div>
 
       <Button
